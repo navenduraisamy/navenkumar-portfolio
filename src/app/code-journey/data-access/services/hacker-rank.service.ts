3b06d4caad6d6b041740db4ebdc6e527853927d2
NAVENKUMAR D <naven.duraisamy5859@gmail.com>
2023-12-28 15:30:09 +0530

    filtered response

--- src/app/code-journey/data-access/services/hacker-rank.service.ts
+++ src/app/code-journey/data-access/services/hacker-rank.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 import { HackerRankCertificate } from '../../types/hacker-rank';
 
 @Injectable()
 export class HackerRankService {
+  private base_url = `hackerrank/community/v1`;
 
-  private base_url = "hackerrank/community/v1"
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getUserCertificates(username: string): Observable<HackerRankCertificate[]>{
-    let params = new HttpParams().set("username", username);
-    return this.http.get<HackerRankCertificate[]>(`${this.base_url}/test_results/hacker_certificate`, { params });
+  getUserCertificates(username: string): Observable<HackerRankCertificate[]> {
+    let params = new HttpParams().set('username', username);
+    return this.http
+      .get<{ data: HackerRankCertificate[]}>(
+        `${this.base_url}/test_results/hacker_certificate`,
+        { params }
+      )
+      .pipe(
+        map(response => response.data),
+        map((items: HackerRankCertificate[]) =>
+          items.filter((item) => item.attributes.status === 'test_passed')
+        ),
+        tap(console.log)
+      );
   }
 }
